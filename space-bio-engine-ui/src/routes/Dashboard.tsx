647e5d8bdee3b1cff8f2c52d1638b424c6d644ae
Vahin Pichairaj <vahin.pichairaj@gmail.com>
2025-10-04 10:52:03 -0500

    Base File Struct

--- space-bio-engine-ui/src/routes/Dashboard.tsx
+++ space-bio-engine-ui/src/routes/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import FilterBar from '../components/FilterBar'
 import ResultsPlaceholder from '../components/ResultsPlaceholder'
 import ChatbotDrawer from '../components/ChatbotDrawer'
